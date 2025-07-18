b73f9fb22d62d0c2e0cdcc4f73565e2300d27295
Abdul Kehmorkai Kamara <abdulkehmorkaikamara@gmail.com>
2025-07-18 17:21:42 +0300

    fix: Add production SSL config for database

--- server/db.js
+++ server/db.js
@@ -1,12 +1,22 @@
-const Pool = require('pg').Pool
-require('dotenv').config()
+// server/db.js
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DBPORT,
-  database: 'todoapp'
-})
+// This configuration object will be used to connect to the database.
+const dbConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
 
-module.exports = pool
+// --- THIS IS THE CRUCIAL PART ---
+// When the application is running in a production environment (like Render),
+// it needs to use SSL to connect to the database securely.
+// Render sets the NODE_ENV variable to 'production' automatically.
+if (process.env.NODE_ENV === 'production') {
+  dbConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const pool = new Pool(dbConfig);
+
+module.exports = pool;
